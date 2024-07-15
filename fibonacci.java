3e2d5c22908b2123c7ce4603f62715182a526469
Somanath10may <somdev12000@gmail.com>
2024-07-15 15:36:08 +0530

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -9,6 +9,7 @@ for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed
 n3=n1+n2;    
 System.out.print(" "+n3); 
 System.out.print("hi");
+System.out.print("hi");
 n1=n2;    
 n2=n3;    
 }    
